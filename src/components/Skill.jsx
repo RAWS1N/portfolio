e20f7d3455ac8cb40f11f06963a6ca731799f84f
RAWSIN <diveshkumarsingariya@gmail.com>
2022-11-21 20:56:38 +0530

    skill animation removed

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -11,7 +11,8 @@ function Skill({ directionLeft, image, name, style, prof }) {
           height="400"
           width="400"
           initial={{
-            x: directionLeft === true ? -200 : 200,
+            // x: directionLeft === true ? -200 : 200,
+            x:0
             opacity: 0,
           }}
           transition={{
